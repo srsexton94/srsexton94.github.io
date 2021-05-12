13a73619b54b76324b0b48f5e728942e252bdc29
srsexton94 <srsexton94@gmail.com>
2021-05-12 18:49:57 -0400

    factor out src paths to shared folder

--- src/app/under-construction/under-construction.component.ts
+++ src/app/under-construction/under-construction.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { faExternalLinkAlt, faPaintRoller, faFilePdf, faTools } from '@fortawesome/free-solid-svg-icons'
-
+import { PATHS } from '../../models/paths'
 @Component({
   selector: 'under-construction',
   styleUrls: ['./under-construction.component.scss'],
@@ -21,51 +21,55 @@ import { faExternalLinkAlt, faPaintRoller, faFilePdf, faTools } from '@fortaweso
       <ul>
         <li>
           Check out
-          <a target="_blank" rel="noopener noreferer" href="https://github.com/srsexton94">
+          <a target="_blank" rel="noopener noreferer" [href]="githubPath">
             my code on Github
             <fa-icon class="icon newtab" aria-label="opens new tab" [icon]="newTabIcon"></fa-icon>
           </a>
         </li>
         <li>
           Connect with me on
-          <a target="_blank" rel="noopener noreferer" href="https://www.linkedin.com/in/samantha-sexton/">
+          <a target="_blank" rel="noopener noreferer" [href]="linkedInPath">
             LinkedIn
             <fa-icon class="icon newtab" aria-label="opens new tab" [icon]="newTabIcon"></fa-icon>
           </a>
         </li>
         <li>
           Review
-          <a target="_blank" rel="noopener noreferer" [href]="pdfPath">
+          <a target="_blank" rel="noopener noreferer" [href]="resumePath">
             my Resume
             <fa-icon class="icon pdf" aria-label="opens pdf on new tab" [icon]="pdfIcon"></fa-icon>
           </a>
         </li>
         <li>
           Check out my favorite past projects:<br>
-          <a target="_blank" rel="noopener noreferer" href="https://connietran-dev.github.io/janebox-surviral-client/#/">
+          <a target="_blank" rel="noopener noreferer" [href]="surViralSitePath">
             SurViral
             <fa-icon class="icon newtab" aria-label="opens new tab" [icon]="newTabIcon"></fa-icon>
           </a>
           and
-          <a target="_blank" rel="noopener noreferer" href="https://srsexton94.github.io/moodtracker-client/">
+          <a target="_blank" rel="noopener noreferer" [href]="moodTrackerSitePath">
             MoodTracker
             <fa-icon class="icon newtab" aria-label="opens new tab" [icon]="newTabIcon"></fa-icon>
           </a>
         </li>
         <li>
-          Email me at <a href="mailto:srsexton94@gmail.com">srsexton94@gmail.com</a>
+          Email me at <a [href]="emailPath">srsexton94@gmail.com</a>
         </li>
       </ul>
-      <img alt="cartoon person working on laptop in kitchen" [src]="underConstructionImg">
+      <img alt="cartoon person working on laptop in kitchen" [src]="blushImage">
     </section>
   `
 })
 export class UnderConstructionComponent {
+  blushImage: string = PATHS.blushImg
+  emailPath: string = PATHS.email
+  githubPath: string = PATHS.github
+  linkedInPath: string = PATHS.linkedin
+  moodTrackerSitePath: string = PATHS.moodTrackerSite
   newTabIcon: IconProp = faExternalLinkAlt
   paintIcon: IconProp = faPaintRoller
   pdfIcon: IconProp = faFilePdf
-  pdfPath: string = '../../assets/documents/SamanthaSexton_SoftwareEngineer_Resume.pdf'
+  resumePath: string = PATHS.resume
+  surViralSitePath: string = PATHS.surViralSite
   toolsIcon: IconProp = faTools
-  underConstructionImg: string = 'https://blush.design/api/download?shareUri=THKY6D0pRbyCdz2x&c=Hair_0%7Ee7b460-0.8%7E372310_Skin_0%7E673a18-0.8%7Ef4d4b8&bg=59eeff&w=800&h=800&fm=png'
-  // edit img at https://blush.design/collections/little-things/scenes-kitchen/THKY6D0pRbyCdz2x?c=Hair_0%7Ee7b460-0.8%7E372310_Skin_0%7E673a18-0.8%7Ef4d4b8&bg=59eeff
 }
---
+++ src/models/paths.ts
@@ -0,0 +1,10 @@
+export enum PATHS {
+  github = 'https://github.com/srsexton94',
+  linkedin = 'https://www.linkedin.com/in/samantha-sexton/',
+  resume = '../assets/documents/SamanthaSexton_SoftwareEngineer_Resume.pdf',
+  blushImg = 'https://blush.design/api/download?shareUri=THKY6D0pRbyCdz2x&c=Hair_0%7Ee7b460-0.8%7E372310_Skin_0%7E673a18-0.8%7Ef4d4b8&bg=59eeff&w=800&h=800&fm=png',
+  blushImgEditor = 'https://blush.design/collections/little-things/scenes-kitchen/THKY6D0pRbyCdz2x?c=Hair_0%7Ee7b460-0.8%7E372310_Skin_0%7E673a18-0.8%7Ef4d4b8&bg=59eeff',
+  surViralSite = 'https://connietran-dev.github.io/janebox-surviral-client/#/',
+  moodTrackerSite = 'https://srsexton94.github.io/moodtracker-client/',
+  email = 'mailto:srsexton94@gmail.com'
+}
