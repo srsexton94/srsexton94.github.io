57fe89777844c7fd70cf0e6fc38a48beaf339e93
srsexton94 <srsexton94@gmail.com>
2021-05-18 22:51:51 -0400

    Fix broken image src

--- src/models/paths/media.ts
+++ src/models/paths/media.ts
@@ -12,7 +12,7 @@ interface IMediaPaths {
 export const MediaPaths: IMediaPaths = {
   aboutMeImg: '../../assets/images/about-me.jpeg',
   bjjVideo: '../../assets/videos/tap-cancer-out.mp4',
-  blushImg: 'https://blush.design/api/download?shareUri=THKY6D0pRbyCdz2x&c=Hair_0%7Ee7b460-0.8%7E372310_Skin_0%7E673a18-0.8%7Ef4d4b8&bg=59eeff&w=800&h=800&fm=png',
+  blushImg: '../../assets/images/the-little-things-kitchen.png',
   blushImgEditor: 'https://blush.design/collections/little-things/scenes-kitchen/THKY6D0pRbyCdz2x?c=Hair_0%7Ee7b460-0.8%7E372310_Skin_0%7E673a18-0.8%7Ef4d4b8&bg=59eeff',
   danceVideo: '../../assets/videos/bolero-finals.mp4',
   pronounBadge: '../../assets/images/pronouns.png',
