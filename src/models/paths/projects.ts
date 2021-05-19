7f2e60074c1db87ed85ed4dd05ccaffd35414d21
srsexton94 <srsexton94@gmail.com>
2021-05-18 21:54:52 -0400

    Add site, code, and image paths for all projects

--- src/models/paths/projects.ts
+++ src/models/paths/projects.ts
@@ -1,6 +1,12 @@
 export enum ProjectTypes {
-  SURVIRAL = 'surViral',
+  BUDGET = 'budgetTracker',
+  CALCULATOR = 'calculator',
   MOODTRACKER = 'moodTracker',
+  POMODORO = 'pomodoro',
+  RUSSIAN = 'russian',
+  RAIN = 'rain',
+  SURVIRAL = 'surViral',
+  TODONE = 'toDone',
   TBD = 'TBD'
 }
 
@@ -14,19 +20,49 @@ interface IProject {
 type IProjectPaths = { [key in ProjectTypes]: IProject }
 
 export const ProjectPaths: IProjectPaths = {
-  surViral: {
-    site: 'https://connietran-dev.github.io/janebox-surviral-client/#/',
-    code: '',
-    image: ''
+  budgetTracker: {
+    site: 'https://chrome.google.com/webstore/detail/kefbibfkinkpmcfdehfmdedogekjbpde/publish-accepted?authuser=0&hl=es',
+    code: 'https://github.com/srsexton94/budget-tracker',
+    image: '../assets/images/projects/budget-tracker.jpeg'
+  },
+  calculator: {
+    site: 'https://srsexton94.github.io/calculator-client/',
+    code: 'https://github.com/srsexton94/calculator-client',
+    image: '../assets/images/projects/calculator.png'
   },
   moodTracker: {
     site: 'https://srsexton94.github.io/moodtracker-client/',
-    code: '',
-    image: ''
+    code: 'https://github.com/srsexton94/moodtracker-client',
+    image: '../assets/images/projects/moodtracker.png'
+  },
+  pomodoro: {
+    site: 'https://srsexton94.github.io/pomodoro/',
+    code: 'https://github.com/srsexton94/pomodoro',
+    image: '../assets/images/projects/pomodoro.jpeg'
+  },
+  rain: {
+    site: 'https://right-as-rain.netlify.app/',
+    code: 'https://github.com/srsexton94/right-as-rain',
+    image: '../assets/images/projects/rain.png'
+  },
+  russian: {
+    site: 'https://srsexton94.github.io/russkaya-klaviatura/',
+    code: 'https://github.com/srsexton94/russkaya-klaviatura',
+    image: '../assets/images/projects/russian.png'
+  },
+  surViral: {
+    site: 'https://connietran-dev.github.io/janebox-surviral-client/#/',
+    code: 'https://github.com/connietran-dev/janebox-surviral-client',
+    image: '../assets/images/projects/surviral.png'
+  },
+  toDone: {
+    site: 'https://srsexton94.github.io/to-done/',
+    code: 'https://github.com/srsexton94/to-done',
+    image: '../assets/images/projects/to-done.png'
   },
   TBD: {
     site: '',
     code: '',
-    image: '../../assets/images/TBD.jpeg'
+    image: '../../assets/images/projects/TBD.jpeg'
   }
 }
