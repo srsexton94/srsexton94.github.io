c898f74b03fb7e6996a41ddd7f2ba7910e2e6849
srsexton94 <srsexton94@gmail.com>
2021-05-31 20:53:53 -0400

    Migrate main-info content to new component

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,48 +1,12 @@
-import { Component } from '@angular/core';
-import { MediaPaths, SocialLinkTypes } from '../../../../src/models/index'
+import { Component } from '@angular/core'
+import { MediaPaths } from '../../../../src/models/index'
 
 @Component({
   selector: 'about-page',
   styleUrls: ['./about.component.scss'],
   template: `
     <section id="about-section" class="page-section">
-      <div class="main-intro">
-        <div class="photo-wrapper">
-          <img class="selfie" [src]="selfieSrc"/>
-          <img class="pronoun-badge" [src]="pronounSrc"/>
-        </div>
-        <div class="self-description">
-          <div class="intro">
-            <h2 class="header">Hi there! I'm Sam</h2>
-            <p class="description">
-              I am a software engineer who thrives off of building and designing human-centered digital
-              experiences that are equal parts accessible and exceptional.
-            </p>
-            <social-link [linkType]="socialLinks.RESUME" [excludeIcon]="true">
-              <img class="resume-preview" aria-hidden="true" [src]="resumeImg"/>  
-            </social-link>
-            <p class="description">
-              From studying the intricacies of language at 
-              <social-link [linkType]="socialLinks.UMASS">
-                UMass Linguistics
-              </social-link>, 
-              to my career in Community Advocacy and Activism,
-              to my current obsessions with 
-              <social-link [linkType]="socialLinks.CLEAN_CODE">
-                Clean code 
-              </social-link> and
-              <social-link [linkType]="socialLinks.CODE_KATA">
-                Code katas 
-              </social-link> &ndash;
-              I have long been fascinated by the nuances of the many complex systems that guide our lives.
-            </p>
-            <p class="description">
-              At the heart of those systems, however, is always people. And I am thrilled to play my part
-              in the movement towards Equity-centered Design and radical accesibility in tech.
-            </p>
-          </div>
-        </div>
-      </div>
+      <main-intro></main-intro>
       <div class="more-aboutme">
         <div class="video-btn-container">
           <button (click)="toggleVideo('BJJ')">BJJ video</button>
@@ -61,11 +25,6 @@ import { MediaPaths, SocialLinkTypes } from '../../../../src/models/index'
   `
 })
 export class AboutComponent {
-  socialLinks: typeof SocialLinkTypes = SocialLinkTypes
-  selfieSrc: string = MediaPaths.aboutMeImg
-  pronounSrc: string = MediaPaths.pronounBadge
-  resumeImg: string = MediaPaths.resumeImg
-
   isVideoShown: { [key: string]: boolean } = {
     BJJ: false,
     DANCE: false
--- src/app/pages/about/main-intro/main-intro.component.ts
+++ src/app/pages/about/main-intro/main-intro.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { MediaPaths, SocialLinkTypes } from '../../../../../src/models/index'
 
 @Component({
   selector: 'main-intro',
   styleUrls: ['./main-intro.component.scss'],
   template: `
+    <div class="main-intro">
+      <div class="photo-wrapper">
+        <img class="selfie" [src]="selfieSrc"/>
+        <img class="pronoun-badge" [src]="pronounSrc"/>
+      </div>
+      <div class="self-description">
+        <div class="intro">
+          <h2 class="header">Hi there! I'm Sam</h2>
+          <p class="description">
+            I am a software engineer who thrives off of building and designing human-centered digital
+            experiences that are equal parts accessible and exceptional.
+          </p>
+          <social-link [linkType]="socialLinks.RESUME" [excludeIcon]="true">
+            <img class="resume-preview" aria-hidden="true" [src]="resumeImg"/>  
+          </social-link>
+          <p class="description">
+            From studying the intricacies of language at 
+            <social-link [linkType]="socialLinks.UMASS">
+              UMass Linguistics
+            </social-link>, 
+            to my career in Community Advocacy and Activism,
+            to my current obsessions with 
+            <social-link [linkType]="socialLinks.CLEAN_CODE">
+              Clean code 
+            </social-link> and
+            <social-link [linkType]="socialLinks.CODE_KATA">
+              Code katas 
+            </social-link> &ndash;
+            I have long been fascinated by the nuances of the many complex systems that guide our lives.
+          </p>
+          <p class="description">
+            At the heart of those systems, however, is always people. And I am thrilled to play my part
+            in the movement towards Equity-centered Design and radical accesibility in tech.
+          </p>
+        </div>
+      </div>
+    </div>
   `
 })
 export class MainIntroComponent {
+  socialLinks: typeof SocialLinkTypes = SocialLinkTypes
+  selfieSrc: string = MediaPaths.aboutMeImg
+  pronounSrc: string = MediaPaths.pronounBadge
+  resumeImg: string = MediaPaths.resumeImg
 }
