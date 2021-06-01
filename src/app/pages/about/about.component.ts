dd75912544901fec9b06dd104039fe2a838a920a
srsexton94 <srsexton94@gmail.com>
2021-05-31 20:39:07 -0400

    Format AboutMe text around resume link

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -15,45 +15,48 @@ import { MediaPaths, SocialLinkTypes } from '../../../../src/models/index'
           <div class="intro">
             <h2 class="header">Hi there! I'm Sam</h2>
             <p class="description">
-              I am a software engineer who thrives off of building and designing human-centered, digital
-              experiences that are equal parts exceptional and accessible.
+              I am a software engineer who thrives off of building and designing human-centered digital
+              experiences that are equal parts accessible and exceptional.
             </p>
+            <social-link [linkType]="socialLinks.RESUME" [excludeIcon]="true">
+              <img class="resume-preview" aria-hidden="true" [src]="resumeImg"/>  
+            </social-link>
             <p class="description">
               From studying the intricacies of language at 
               <social-link [linkType]="socialLinks.UMASS">
-                UMass linguistics
+                UMass Linguistics
               </social-link>, 
-              to my career in community advocacy and activism, to my current obsession with 
+              to my career in Community Advocacy and Activism,
+              to my current obsessions with 
               <social-link [linkType]="socialLinks.CLEAN_CODE">
                 Clean code 
               </social-link> and
               <social-link [linkType]="socialLinks.CODE_KATA">
                 Code katas 
               </social-link> &ndash;
-              I am fascinated by the nuances of the complex systems that guide our lives.
+              I have long been fascinated by the nuances of the many complex systems that guide our lives.
             </p>
             <p class="description">
               At the heart of those systems, however, is always people. And I am thrilled to play my part
-              in the movement towards Equity-centered design and radical accesibility in tech.
+              in the movement towards Equity-centered Design and radical accesibility in tech.
             </p>
-          </div>
-          <social-link [linkType]="socialLinks.RESUME" [excludeIcon]="true">
-            <img class="resume-preview" [src]="resumeImg"/>
-          </social-link>
-          <div class="video-btn-container">
-            <button (click)="toggleVideo('BJJ')">BJJ video</button>
-            <button (click)="toggleVideo('DANCE')">Dance video</button>
           </div>
         </div>
       </div>
-      <video *ngIf="isVideoShown.BJJ" controls>
-        <source type="video/mp4" [src]="getVideoSrc('BJJ')">
-        Your browser does not support the video tag.
-      </video>
-      <video *ngIf="isVideoShown.DANCE" controls>
-        <source type="video/mp4" [src]="getVideoSrc('DANCE')">
-        Your browser does not support the video tag.
-      </video>
+      <div class="more-aboutme">
+        <div class="video-btn-container">
+          <button (click)="toggleVideo('BJJ')">BJJ video</button>
+          <button (click)="toggleVideo('DANCE')">Dance video</button>
+        </div>
+        <video *ngIf="isVideoShown.BJJ" controls>
+          <source type="video/mp4" [src]="getVideoSrc('BJJ')">
+          Your browser does not support the video tag.
+        </video>
+        <video *ngIf="isVideoShown.DANCE" controls>
+          <source type="video/mp4" [src]="getVideoSrc('DANCE')">
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </section>
   `
 })
