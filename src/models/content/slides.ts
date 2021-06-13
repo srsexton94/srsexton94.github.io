580d5c8dfa7906f04d6ec05f4f751d5d6700dfb2
srsexton94 <srsexton94@gmail.com>
2021-06-13 11:44:46 -0400

    Edit slide text to meet line length max

--- src/models/content/slides.ts
+++ src/models/content/slides.ts
@@ -12,56 +12,80 @@ export const CommunitySlides: ISlide[] = [
     image: '../../assets/images/slides/boston-cityscape.jpeg',
     altText: 'Nighttime cityscape of boston over water with bright streetlamp',
     heading: 'Police Misconduct Data Trust',
-    body: 'I contribute as a Software Engineer and UX designer to help build a national index of police incidents through Code for Boston',
+    body: `
+      I contribute as a Software Engineer and UX designer to help build a national index of
+      police incidents through Code for Boston & The Tubman Project.
+    `,
     isActive: true
   },
   {
     image: '../../assets/images/slides/woc-coding.jpg',
     altText: 'Three people seated on a couch and working on laptops',
     heading: 'G { Code }',
-    body: 'I serve as a mentor in support of women and non-binary people of color pursuing careers as change makers in the tech industry',
+    body: `
+      I serve as a mentor in support of women and non-binary people of color pursuing careers as
+      change makers in the tech industry.
+    `,
     isActive: false
   },
   {
     image: '../../assets/images/slides/code-for-boston.jpeg',
     altText: 'Six people working on laptops around a conference table and talking',
     heading: 'Clean Slate',
-    body: 'I helped develop and design an app to help those applying for sealing/expungement of criminal records through Code for Boston',
+    body: `
+      I helped develop an app to help those applying for sealing/expungement of criminal records through
+      Code for Boston & Citizens for Juvenile Justice.
+    `,
     isActive: false
   },
   {
     image: '../../assets/images/slides/amherst-survival-center.png',
     altText: 'A person serves food at a lunch counter while two others work behind them in a kitchen',
     heading: 'Amherst Survival Center',
-    body: 'I helped prepare high-volume, free, healthy lunches for the local community and translated for Spanish & Russian speaking guests.',
+    body: `
+      I helped prepare high-volume, free, healthy lunches for the local community, facilitated grocery
+      distribution, and translated for Spanish, Russian, & A.S.L. speaking guests.
+    `,
     isActive: false
   },
   {
     image: '../../assets/images/slides/shaha-storytellers.jpg',
     altText: 'A large group wearing matching cast t-shirts pose theatrically',
     heading: 'Shaha: The Storytellers',
-    body: 'I wrote, performed, and facilitated theatrical workshops on issues of social injustice & oppression through storytelling',
+    body: `
+      I wrote, performed, and facilitated theatrical workshops on issues of social injustice & oppression
+      through storytelling - helping to lead multi-year series on Racism in LGBTQ+ spaces.
+    `,
     isActive: false
   },
   {
     image: '../../assets/images/slides/stonewall.jpeg',
     altText: 'A red couch with rainbow pillows sits in front of a bookshelf full of LGBTQ+ titles',
     heading: 'Stonewall Center',
-    body: 'I coordinated a panel based series speaking to the diversity and humanity of the LGBTQ+ community and supported LGBTQ+ students',
+    body: `
+      I coordinated a panel based series speaking to the diversity and humanity of the LGBTQ+ community
+      and supported LGBTQ+ students.
+    `,
     isActive: false
   },
   {
     image: '../../assets/images/slides/center-women-community.jpeg',
-    altText: 'A black trifold poster reads "CWC we\'ve got your back" with 5 multicolored silohettes standing on grass',
+    altText: 'A black trifold standing on grass reads "CWC we\'ve got your back" above 5 colorful silohettes',
     heading: 'Center for Women & Community',
-    body: 'I served as an Educator Advocate, facilitating professional trainings on Intimate Partner Violence Prevention and supporting survivors',
+    body: `
+      I served as an Educator Advocate, facilitating professional trainings on Intimate Partner Violence
+      and supporting survivors.
+    `,
     isActive: false
   },
   {
     image: '../../assets/images/slides/working-at-desk.jpg',
     altText: 'Three people seated at a table with laptops talking and laughing',
     heading: 'Design X Humanity',
-    body: 'I worked as a UX Engineer & Instructional Designer to help build educational tools for K-12 students on critical anti-racism and social equity',
+    body: `
+      I worked as a UX Engineer & Instructional Designer to help build educational tools for K-12 students
+      on critical anti-racism and social equity.
+    `,
     isActive: false
   }
 ]
