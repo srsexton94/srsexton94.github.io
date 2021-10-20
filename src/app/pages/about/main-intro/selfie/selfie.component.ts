8a47a6bd3ac969feef9e05fa3ccb3939b42afd15
Samantha Sexton <50561643+srsexton94@users.noreply.github.com>
2021-10-20 12:15:36 -0400

    Update resume and image alts

--- src/app/pages/about/main-intro/selfie/selfie.component.ts
+++ src/app/pages/about/main-intro/selfie/selfie.component.ts
@@ -5,13 +5,17 @@ import { IImagePaths, ImagePaths } from '../../../../../../src/models/index'
   selector: 'selfie',
   styleUrls: ['./selfie.component.scss'],
   template: `
-    <div class="photo-wrapper">
-      <img class="selfie" [src]="images.aboutMeImg"/>
-      <img class="pronoun-badge" [src]="images.pronounBadge"/>
-      <img class="aws-ccp" [src]="images.awsCCP"/>
-    </div>
+    <section class="photo-wrapper">
+      <img class="selfie" [src]="images.aboutMeImg" [attr.aria-label]="describeSelfie"/>
+      <img class="pronoun-badge" [src]="images.pronounBadge" [attr.aria-label]="describePronouns"/>
+      <img class="aws-ccp" [src]="images.awsCCP" [attr.aria-label]="describeAWS"/>
+    </section>
   `
 })
 export class SelfieComponent {
+  describeAWS: string = 'Amazon Web Services Certified Cloud Practitioner completion badge'
+  describePronouns: string = 'Name tag reading \'Hello! My Pronouns are They/Them\''
+  describeSelfie: string = 
+    'Smiling photo of Sam, a white person with short dual tone hair, glasses, and a collared shirt'
   images: IImagePaths = ImagePaths
 }
