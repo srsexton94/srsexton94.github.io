6c5e98922b01fd29fd4ba715fd03f0298f316ac2
Samantha Sexton <50561643+srsexton94@users.noreply.github.com>
2023-06-05 16:39:52 -0400

    remove main-intro component, put text in about

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -5,7 +5,37 @@ import { Component } from '@angular/core'
   styleUrls: ['./about.component.scss'],
   template: `
     <section id="about-me-section" class="page-section">
-      <main-intro></main-intro>
+      <div class="main-intro">
+        <selfie class="desktop-only"></selfie>
+        <div class="self-description">
+          <div class="intro">
+            <h2 class="header">Hi there! I'm Sam</h2>
+            <selfie class="mobile-only"></selfie>
+            <p class="description">
+              I'm a professional frontend Software Engineer and experienced Technical Lead
+              who thrives off of architecting human-centered digital experiences that are
+              equal parts accessible and exceptional. 
+            </p>
+            <resume-preview class="desktop-only"></resume-preview>
+            <p class="description">
+              Working in FinTech, EdTech, CivicTech, and beyond I've honed my skills building
+              responsive and interactive interfaces. I have a considerable depth of knowledge
+              in crafting scalable, maintainable code and a unique capacity to seamlessly build
+              accessibility into the development lifecycle.
+            </p>
+            <p class="description">
+              At the heart of my work is always community &ndash; and I am thrilled to play my 
+              part in the movements towards Equitable Design and radical accesibility in tech.
+            </p>
+            <p class="description">
+              Overall, I know myself to be an analytical, driven, and collaborative engineer
+              who is always excited to learn something new, and unpacks even the most complex
+              problem with diligence and a laugh (or two!).
+            </p>
+            <resume-preview class="mobile-only"></resume-preview>
+          </div>
+        </div>
+      </div>
     </section>
   `
 })
