792fef1b27b7b5cd2cc3d496556782286cad5bac
Samantha Sexton <50561643+srsexton94@users.noreply.github.com>
2023-06-06 11:28:55 -0400

    remove linguist from titles

--- src/models/content/typewriter.ts
+++ src/models/content/typewriter.ts
@@ -1,16 +1,8 @@
 enum DevTitles {
   ENGINEER = 'Full Stack Software Engineer',
-  DESIGNER = 'UX Designer',
   ACCESSIBILITY = 'Web Accessibility Specialist',
+  DESIGNER = 'UX Designer',
   COMMUNITY = 'Community Advocate',
-  LINGUIST = 'Theoretical Linguist'
-}
-
-enum PersonalInterests {
-  BJJ = 'train and compete as a JiuJiteira',
-  DANCE = 'perform and teach as a Ballroom Dancer',
-  LANG = 'try my darndest to learn alllllll the languages!'
 }
 
 export const DevTitleList: string[] = Object.values(DevTitles)
-export const InterestsList: string[] = Object.values(PersonalInterests)
