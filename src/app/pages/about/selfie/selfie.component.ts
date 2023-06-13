a3c70ccfee8aeb3ac54322b090f7f6dc11d0f184
Samantha Sexton <50561643+srsexton94@users.noreply.github.com>
2023-06-13 16:12:54 -0400

    refactor selfie image to reduce repetition

--- src/app/pages/about/selfie/selfie.component.ts
+++ src/app/pages/about/selfie/selfie.component.ts
@@ -7,23 +7,14 @@ import { ISelfieImageDetails, SelfieImageDetails } from '../../../../models';
   template: `
     <section class="photo-wrapper">
       <img
-        [class]="image.selfie.class"
-        [src]="image.selfie.src"
-        [attr.aria-label]="image.selfie.altText"
-      />
-      <img
-        [class]="image.pronounBadge.class"
-        [src]="image.pronounBadge.src"
-        [attr.aria-label]="image.pronounBadge.altText"
-      />
-      <img
-        [class]="image.awsCCP.class"
-        [src]="image.awsCCP.src"
-        [attr.aria-label]="image.awsCCP.altText"
+        *ngFor="let image of images"
+        [class]="image.class"
+        [src]="image.src"
+        [attr.aria-label]="image.altText"
       />
     </section>
   `,
 })
 export class SelfieComponent {
-  image: ISelfieImageDetails = SelfieImageDetails;
+  images: ISelfieImageDetails[] = SelfieImageDetails;
 }
--- src/models/selfie-images.ts
+++ src/models/selfie-images.ts
@@ -1,38 +1,36 @@
 export enum SelfieImageTypes {
   SELFIE = 'selfie',
-  CCP = 'awsCCP',
-  PRONOUNS = 'pronounBadge',
+  CCP = 'aws-ccp',
+  PRONOUNS = 'pronoun-badge',
 }
 
 export type ISelfieImageDetails = {
-  [key in SelfieImageTypes]: {
-    src: string,
-    altText: string,
-    class: string,
-  }
+  class: SelfieImageTypes,
+  src: string,
+  altText: string,
 }
 
-export const SelfieImageDetails: ISelfieImageDetails = {
-  [SelfieImageTypes.SELFIE]: {
+export const SelfieImageDetails: ISelfieImageDetails[] = [
+  { 
+    class: SelfieImageTypes.SELFIE,
     src: '../../assets/images/about-me.jpeg',
     altText: `
       Smiling photo of Sam, a white person with bright pink and orange hair,
       in an open collared shirt against a brick background.
     `,
-    class: 'selfie'
   },
-  [SelfieImageTypes.CCP]: {
-    src: '../../assets/images/aws-ccp.png',
-    altText: `
-      Amazon Web Services Certified Cloud Practitioner completion badge
-    `,
-    class: 'aws-ccp'
-  },
-  [SelfieImageTypes.PRONOUNS]: {
+  { 
+    class: SelfieImageTypes.PRONOUNS,
     src: '../../assets/images/pronouns.png',
     altText: `
       Name tag reading 'Hello! My Pronouns are They/Them'
     `,
-    class: 'pronoun-badge'
   },
-}
+  { 
+    class: SelfieImageTypes.CCP,
+    src: '../../assets/images/aws-ccp.png',
+    altText: `
+      Amazon Web Services Certified Cloud Practitioner completion badge
+    `,
+  },
+];
