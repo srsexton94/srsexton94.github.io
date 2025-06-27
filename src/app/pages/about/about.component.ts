d14822d4fb65dbe51db9071db690650eb6a737bc
srsexton94 <srsexton94@gmail.com>
2025-06-27 12:39:21 -0400

    update about me text

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'about-page',
@@ -12,31 +12,36 @@ import { Component } from '@angular/core'
             <h2 class="header">Hi there! I'm Sam</h2>
             <selfie class="mobile-only"></selfie>
             <p class="description">
-              I'm a professional frontend Software Engineer and experienced Technical Lead
-              who thrives off of architecting human-centered digital experiences that are
-              equal parts accessible and exceptional. 
+              I'm a professional frontend Software Engineer, UX Designer, and
+              experienced Technical Lead who thrives off of architecting
+              human-centered digital experiences that are equal parts accessible
+              and exceptional.
             </p>
             <resume-preview class="desktop-only"></resume-preview>
             <p class="description">
-              Working in FinTech, EdTech, CivicTech, and beyond I've honed my skills building
-              responsive and interactive interfaces. I have a considerable depth of knowledge
-              in crafting scalable, maintainable code and a unique capacity to seamlessly build
-              accessibility into the development lifecycle.
+              Over years of creating highly performant applications, I have
+              honed my skills in designing and building well-researched,
+              responsive, interactive, and empathetic user interfaces. I have a
+              considerable depth of knowledge in crafting scalable, maintainable
+              systems and a unique capacity to seamlessly build accessibility
+              into the product lifecycle.
             </p>
             <p class="description">
-              At the heart of my work is always community &ndash; and I am thrilled to play my 
-              part in the movements towards Equitable Design and radical accesibility in tech.
+              At the heart of my work is always community &ndash; and I am
+              thrilled to play my part in the movements towards Equitable Design
+              and radical accesibility in tech.
             </p>
             <p class="description">
-              Overall, I know myself to be an analytical, driven, and collaborative engineer
-              who is always excited to learn something new, and unpacks even the most complex
-              problem with diligence and a laugh (or two!).
+              Overall, I know myself to be an analytical, driven, and
+              collaborative technologist who is always excited to learn
+              something new and unpacks even the most complex problem with
+              diligence and a laugh (or two!).
             </p>
             <resume-preview class="mobile-only"></resume-preview>
           </div>
         </div>
       </div>
     </section>
-  `
+  `,
 })
 export class AboutComponent {}
