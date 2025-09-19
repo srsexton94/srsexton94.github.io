7085b53e231cdbbb00181a5e73a1aea37ed632e2
srsexton94 <srsexton94@gmail.com>
2025-09-19 14:25:06 -0400

    remove community slide remnants

--- src/models/typewriter.ts
+++ src/models/typewriter.ts
@@ -2,7 +2,6 @@ enum DevTitles {
   ENGINEER = 'Frontend Software Engineer',
   DESIGNER = 'UX Designer',
   ACCESSIBILITY = 'Web Accessibility Enthusiast',
-  COMMUNITY = 'Community Advocate',
 }
 
 export const DevTitleList: string[] = Object.values(DevTitles);
